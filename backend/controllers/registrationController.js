b43967c79d719d94a4682f26d84b2711ddd16001
Koushik <ykoushik78@gmail.com>
2026-07-29 17:24:02 +0530

    Fix Gmail transporter

--- backend/controllers/registrationController.js
+++ backend/controllers/registrationController.js
@@ -7,9 +7,7 @@ const { deleteFile } = require('../utils/fileUtils');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Use SSL
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
